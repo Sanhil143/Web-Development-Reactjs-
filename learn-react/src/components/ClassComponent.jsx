4c95f1757acdeafa25384c3a05ddcbf495a60214
Sanhil143 <sanhilrai143@gmail.com>
2023-05-26 23:37:53 +0530

    functional & class component

---
+++ learn-react/src/components/ClassComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export class ClassComponent extends React.Component{
+      render(){
+            return(
+                  <h1>I am class component</h1>
+            )
+      }
+}
---
+++ learn-react/src/components/FunctionalComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const FunctionalComponent = () => {
+
+
+      
+  return (
+    <>
+      <h1>
+            I am functional components
+      </h1>
+     
+    </>
+  )
+}
+
+// export default FunctionalComponent
