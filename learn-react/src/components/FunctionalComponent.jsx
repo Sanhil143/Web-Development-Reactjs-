5e4d00ce58cd30b27cc3f6b7ebb59e8bc0fe483e
Sanhil143 <sanhilrai143@gmail.com>
2023-05-28 20:48:43 +0530

    functional

--- learn-react/src/components/FunctionalComponent.jsx
+++ learn-react/src/components/FunctionalComponent.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 export const FunctionalComponent = () => {
 
-
+const handleClick = () => {
+  console.log('button click');
+}
       
   return (
     <>
       <h1>
             I am functional components
       </h1>
+      <button onClick={handleClick}>Click Me</button>
      
     </>
   )
