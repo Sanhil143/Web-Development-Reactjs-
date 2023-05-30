6d9653bd17cd848e008a22920a8d975c7bb46a0a
Sanhil143 <sanhilrai143@gmail.com>
2023-05-31 00:27:25 +0530

    Complete

--- Projects/increment-decrement-assign/src/components/IncrementDecrement.jsx
+++ Projects/increment-decrement-assign/src/components/IncrementDecrement.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './IncDec.css';
 
 const IncrementDecrement = () => {
+      const [data,setData] = useState(0);
+
+
+      const handleClickInc = () => {
+            setData(data + 1);
+      }
+      const handleClickDec = () => {
+            setData(data - 1);
+      }
+      const handleClickInc10 = () => {
+            setData(data + 10);
+      }
+      const handleClickDec10 = () => {
+            setData(data - 10);
+      }
   return (
     <>
      <div className='container'>
-     <h1 style={{textAlign:'center'}}>Increment or Decrement</h1>
-     <button>U</button>
-     <button>D</button>
-     <button>R</button>
-     <button>L</button>
+     <h1>Increment or Decrement</h1>
+     <h2>{data}</h2>
+    <div className='button-container'>
+    <button style={{backgroundColor:'greenyellow'}} onClick={handleClickInc}>UP +1</button>
+     <button style={{backgroundColor:'red'}} onClick={handleClickDec}>DOWN -1</button>
+     <button style={{backgroundColor:'greenyellow'}} onClick={handleClickInc10}>RIGHT +10</button>
+     <button style={{backgroundColor:'red'}} onClick={handleClickDec10}>LEFT -10</button>
+    </div>
      </div> 
     </>
   )
