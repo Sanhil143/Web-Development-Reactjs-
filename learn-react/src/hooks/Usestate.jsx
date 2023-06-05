75ec2ab0ade181d2718b489324175ff3bb2b5059
Sanhil143 <sanhilrai143@gmail.com>
2023-06-05 19:36:29 +0530

    useState hooks

--- learn-react/src/hooks/Usestate.jsx
+++ learn-react/src/hooks/Usestate.jsx
@@ -6,10 +6,10 @@ const Usestate = () => {
     <>
       <h1>Count is {count}</h1>
       <h5>Number is {count % 2 === 0 ? 'Even' : 'Odd'}</h5>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count + 10)}>Increment 10++</button><br/>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(count - 10)}>Decrement 10--</button>
+      <button onClick={() => setCount(count + 1)}>Increment ++</button>
+      <button onClick={() => setCount(count + 10)}>Increment 10 ++</button><br/>
+      <button onClick={() => setCount(count - 1)}>Decrement --</button>
+      <button onClick={() => setCount(count - 10)}>Decrement 10 --</button>
     </>
   )
 }
