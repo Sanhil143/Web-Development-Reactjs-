f9a306ad19320f784afed3b6388767b7ef6d6efa
Sanhil143 <sanhilrai143@gmail.com>
2024-02-21 01:09:15 +0530

    navbar

---
+++ Assignments/web-api/src/static/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <ul className="list">
+          <li>home</li>
+          <li>aboutus</li>
+          <li>blogs</li>
+          <li>contactus</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
