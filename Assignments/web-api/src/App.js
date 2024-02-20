8205326b49769b3823d8af8cfd31b3eb3ab6976e
Sanhil143 <sanhilrai143@gmail.com>
2024-02-21 01:50:55 +0530

    sidebar-running

--- Assignments/web-api/src/App.js
+++ Assignments/web-api/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './static/Navbar';
+import Sidebar from './static/Sidebar';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Sidebar/>
     </>
   );
 }
