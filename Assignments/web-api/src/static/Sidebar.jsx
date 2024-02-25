04ba451754819f8127237b41f956c4429d24aaa3
Sanhil143 <sanhilrai143@gmail.com>
2024-02-25 23:58:38 +0530

    setup profile

--- Assignments/web-api/src/static/Sidebar.jsx
+++ Assignments/web-api/src/static/Sidebar.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Sidebar.css'
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import Data from '../components/Data';
 
 const Sidebar = () => {
+  const [showPost, setShowPost] = useState(false)
+
+  const togglePost = () => {
+    setShowPost(!showPost);
+  }
   return (
     <div className='main'>
       <div className='sidebar'>
     <ol className='sidebar-list'>
-      <li className='side-item' ><Link to='/posts'>Post</Link></li>
+      <li className='side-item' ><Link to='/posts' onClick={togglePost}>Post</Link></li>
       <li className='side-item'><Link to="/profile">Profile</Link></li>
       <li className='side-item'>Setting</li>
       <li className='side-item'>Logout</li>
     </ol>
     </div>
-    <Data/>
+    {showPost && <Data/>}
     </div>
 
   )
