66f0b4969effd979e4c3da28238339462c3d71ef
Sanhil143 <sanhilrai143@gmail.com>
2024-03-01 23:56:00 +0530

    update profile

--- Assignments/web-api/src/static/Sidebar.jsx
+++ Assignments/web-api/src/static/Sidebar.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
 import './Sidebar.css'
-import { Link, Route } from 'react-router-dom';
-import Data from '../components/Data';
+import { Link} from 'react-router-dom';
+import People from '../components/pages/People';
+import Profile from '../components/pages/Profile';
 
 const Sidebar = () => {
   const [showPost, setShowPost] = useState(false)
+  const [showProfile, setShowProfile] = useState(false)
 
   const togglePost = () => {
     setShowPost(!showPost);
+    setShowProfile(!showProfile);
   }
   return (
     <div className='main'>
       <div className='sidebar'>
     <ol className='sidebar-list'>
-      <li className='side-item' ><Link to='/posts' onClick={togglePost}>Post</Link></li>
-      <li className='side-item'><Link to="/profile">Profile</Link></li>
+      <li className='side-item'onClick={togglePost}>Post</li>
+      <li className='side-item' onClick={togglePost}>Profile</li>
       <li className='side-item'>Setting</li>
       <li className='side-item'>Logout</li>
     </ol>
     </div>
-    {showPost && <Data/>}
+    {showPost && <People/>}
+    {showProfile && <Profile/>}
     </div>
 
   )
