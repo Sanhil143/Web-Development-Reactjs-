77b370cfb4a1c97b5757fd1696dc7476b3279d23
Sanhil143 <sanhilrai143@gmail.com>
2024-03-10 01:18:43 +0530

    add navbar

---
+++ Projects/canva-clone/src/static/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <ul className="navbar-ul">
+        <li className="navbar-li">design spotlight</li>
+        <li className="navbar-li">Business</li>
+        <li className="navbar-li">Education</li>
+        <li className="navbar-li">Plans</li>
+        <li className="navbar-li">Learn</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
