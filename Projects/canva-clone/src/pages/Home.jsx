4047a856b24441902589dfc1be6594b3002ba0db
Sanhil143 <sanhilrai143@gmail.com>
2024-03-13 01:04:19 +0530

    update home

--- Projects/canva-clone/src/pages/Home.jsx
+++ Projects/canva-clone/src/pages/Home.jsx
@@ -9,7 +9,12 @@ const Home = () => {
         <button className='san-button'>Custom size</button>
         <button className='san-button'>upload</button>
       </div>
-
+      <div className='san-tagDiv'>
+      <span className='san-tag'>What will you design today?</span>
+      </div>
+      <div className='san-searchbar'>
+      
+      </div>
       </div>
     </div>
   )
