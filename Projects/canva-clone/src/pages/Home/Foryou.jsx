9f1d1dabf54b48c8eb7bfe7ecfe915252d39e5b3
Sanhil143 <sanhilrai143@gmail.com>
2024-03-15 00:36:01 +0530

    add foryou page on home

--- Projects/canva-clone/src/pages/Home/Foryou.jsx
+++ Projects/canva-clone/src/pages/Home/Foryou.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
-
+import './Foryou.css'
 const Foryou = () => {
   return (
     <div>
-      
+      <div className='san-constainer'>
+      <div className='san-car-container'>
+        <div className='san-car'>
+          <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
+          <p className='san-para'>Presentation</p>
+        </div>
+        <div className='san-car'>
+          <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
+          <p className='san-para'>Poster</p>
+        </div>
+        <div className='san-car'>
+          <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
+          <p className='san-para'>Instagram Post</p>
+        </div>
+        <div className='san-car'>
+          <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
+          <p className='san-para'>Resume</p>
+        </div>
+        <div className='san-car'>
+          <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
+          <p className='san-para'>Flyer</p>
+        </div>
+        <div className='san-car'>
+          <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
+          <p className='san-para'>Resume</p>
+        </div>
+      </div>
+      </div>
     </div>
   )
 }
