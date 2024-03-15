344e93413f18ca99b1c6a8ce08063d46605b8dc0
Sanhil143 <sanhilrai143@gmail.com>
2024-03-15 23:59:45 +0530

    update foryou page

--- Projects/canva-clone/src/pages/Home/Foryou.jsx
+++ Projects/canva-clone/src/pages/Home/Foryou.jsx
@@ -4,7 +4,7 @@ const Foryou = () => {
   return (
     <div>
       <div className='san-constainer'>
-      <h5 className='san-h5'>You might want tot try...</h5>
+      <h5 className='san-h5'>You might want to try...</h5>
       <div className='san-car-container'>
         <div className='san-car'>
           <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
@@ -31,6 +31,9 @@ const Foryou = () => {
           <p className='san-para'>Resume</p>
         </div>
       </div>
+      </div>
+      <div className='san-container'>
+        <h5>Recent Designs</h5>
       </div>
     </div>
   )
