a44df20d178ec59a2a8b427e522731cf43c15b92
Sanhil143 <sanhilrai143@gmail.com>
2024-03-16 01:06:06 +0530

    complete for you

--- Projects/canva-clone/src/pages/Home/Foryou.jsx
+++ Projects/canva-clone/src/pages/Home/Foryou.jsx
@@ -3,7 +3,7 @@ import './Foryou.css'
 const Foryou = () => {
   return (
     <div>
-      <div className='san-constainer'>
+      <div className='san-container'>
       <h5 className='san-h5'>You might want to try...</h5>
       <div className='san-car-container'>
         <div className='san-car'>
@@ -32,8 +32,30 @@ const Foryou = () => {
         </div>
       </div>
       </div>
-      <div className='san-container'>
-        <h5>Recent Designs</h5>
+      <div className='san-container2'>
+        <h5 className='san-h5'>Recent Designs</h5>
+        <div className='san-car-container'>
+        <div className='san-car'>
+          <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
+          <p className='san-para'>Sanhil resume</p>
+        </div>
+        <div className='san-car'>
+          <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
+          <p className='san-para'>ashi docs</p>
+        </div>
+        <div className='san-car'>
+          <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
+          <p className='san-para'>shiv images</p>
+        </div>
+        <div className='san-car'>
+          <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
+          <p className='san-para'>madhur resume</p>
+        </div>
+        <div className='san-car'>
+          <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
+          <p className='san-para'>resume's</p>
+        </div>
+      </div>
       </div>
     </div>
   )
