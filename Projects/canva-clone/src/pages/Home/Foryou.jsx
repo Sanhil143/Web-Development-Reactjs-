df90dee6602d2112644622f82798cb859a8617f2
Sanhil143 <sanhilrai143@gmail.com>
2024-03-18 23:28:15 +0530

    update setup

--- Projects/canva-clone/src/pages/Home/Foryou.jsx
+++ Projects/canva-clone/src/pages/Home/Foryou.jsx
@@ -24,11 +24,11 @@ const Foryou = () => {
         </div>
         <div className='san-car'>
           <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
-          <p className='san-para'>Flyer</p>
+          <p className='san-para'>Flyer1</p>
         </div>
         <div className='san-car'>
           <img className='san-img' src='https://plus.unsplash.com/premium_photo-1661573322713-0935eae5f6a9?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='presentation-img'></img>
-          <p className='san-para'>Resume</p>
+          <p className='san-para'>Resumes</p>
         </div>
       </div>
       </div>
